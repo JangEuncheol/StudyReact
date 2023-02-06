f43274c9269999e3aef531ddbe0ad034bdb52460
Eun <euncheol.jang@howoocast.com>
2023-02-06 17:23:24 +0900

    음식 주문 다시

---
+++ section-11/src/components/UI/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./Card.module.css";
+
+const Card = (props) => {
+  const data = props.data;
+  return (
+    <div className={classes.card}>
+      {props.children}
+      {/* <h3>{data.title}</h3>
+      <p className={classes.description}>{data.desc}</p>
+      <p className={classes.price}>{`${data.price}$`}</p> */}
+    </div>
+  );
+};
+
+export default Card;
