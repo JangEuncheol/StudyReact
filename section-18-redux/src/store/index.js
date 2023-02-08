5717c65e054bc345b9b3085ce2e20348337fc721
Eun <euncheol.jang@howoocast.com>
2023-02-08 10:44:18 +0900

    toolkit으로 마이그레이션 시작

---
+++ section-18-redux/src/store/index.js
@@ -0,0 +1,68 @@
+// import { createStore } from "redux";
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+
+export const INCREMENT = 'increment';
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === INCREMENT) {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter
+//     };
+//   }
+
+//   if (action.type === "increse") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter
+//     };
+//   }
+
+//   if (action.type === 'toggle') {
+//     return {
+//       counter: state.counter,
+//       showCounter: !state.showCounter
+//     }
+//   }
+
+//   return state;
+// };
+
+// const store = createStore(counterSlice.reducer);
+
+
+const store = configureStore({
+  reducer: counterSlice.reducer
+});
+
+export const counterActions = counterSlice.actions;
+
+export default store;
