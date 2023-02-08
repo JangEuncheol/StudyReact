7645720a448982c83da04f3c6e8cf03de0993751
Eun <euncheol.jang@howoocast.com>
2023-02-08 17:25:21 +0900

    Todo제거 연습

--- section-27-ts/src/components/TodoItem.tsx
+++ section-27-ts/src/components/TodoItem.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import Todo from "../models/todo";
+import classes from "./TodoItem.module.css"
 
-const TodoItem: React.FC<{data: Todo}> = props => {
-  console.log(props)
-  return <li>
-    {props.data.text}
+const TodoItem: React.FC<{text: string}> = props => {
+  return <li className={classes.item}>
+    {props.text}
   </li>
 };
 
